70d3045e64d0dfe148fc9ffabe49e95ff6285df1
Bing-Xuan-Lu <44064284+Bing-Xuan-Lu@users.noreply.github.com>
2025-04-17 22:35:25 +0800

    設定調整

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -19,7 +19,6 @@
         <header class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6 not-has-[nav]:hidden">
             @if (Route::has('login'))
                 <nav class="flex items-center justify-end gap-4">
-                    @breakpoint();
                     @auth
                         <a
                             href="{{ url('/dashboard') }}"
